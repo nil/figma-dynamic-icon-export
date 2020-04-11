0fee6e115192bc75f7cd8240c751863013b8f110
Nil Vila <nilvilamar@gmail.com>
2020-04-11 18:39:04 +0200

    Simpify error message

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -16,10 +16,10 @@ class ErrorMessage extends React.Component<Props, {}> {
           <path fillRule="evenodd" d="M12.988 3.756A1.5 1.5 0 0114.29 3h3.42a1.5 1.5 0 011.302.756l11.703 20.48a1.5 1.5 0 01-.054 1.576l-1.68 2.52a1.5 1.5 0 01-1.249.668H4.268a1.5 1.5 0 01-1.249-.668l-1.68-2.52a1.5 1.5 0 01-.054-1.576l11.703-20.48zM14.58 5L3.175 24.96 4.535 27h22.93l1.36-2.04L17.42 5h-2.84zm-.08 16h3v3h-3v-3zm2.48-3v-7h-1.96v7h1.96z" />
         </svg>
         <div className="error-info">
-          <div className="error-name type type--pos-large-bold">{entry.name}</div>
+          <div className="error-name type type--pos-large-bold" onClick={() => this.viewNode(entry.name)}>{entry.name}</div>
           <div className="error-context type type--pos-medium-normal">
-            <span className="error-type">Error type: {entry.type}</span> ·
-            <span className="error-view" onClick={() => this.viewNode(entry.name)}> View frame</span>
+            <span className="error-notify">Error type: </span>
+            <span className="error-type">{entry.type}</span>
           </div>
         </div>
       </div>
