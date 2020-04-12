495e2c976414dae11d81866ff0071becd9d08780
Nil Vila <nilvilamar@gmail.com>
2020-04-12 16:11:41 +0200

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.json'
   },
   plugins: ['@typescript-eslint'],
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript'
   ],
   rules: {
+    'comma-dangle': ['error', 'never'],
+    'no-param-reassign': 0,
   }
 }
