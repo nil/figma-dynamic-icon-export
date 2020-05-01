9e2f6bb125a7b763480d17fe0a1e5809da2fb482
Nil Vila <nilvilamar@gmail.com>
2020-05-01 14:29:28 +0200

    Update NodeCheckbox markup

--- src/components/NodeCheckbox.tsx
+++ src/components/NodeCheckbox.tsx
@@ -14,9 +14,28 @@ const NodeCheckbox = ({ node }: Props): JSX.Element => {
     // parent.postMessage({ pluginMessage: { [content]: true } }, '*');
   };
 
+  const viewNode = (id: string): void => {
+    parent.postMessage({ pluginMessage: { viewNodes: id } }, '*');
+  };
+
   return (
-    <div onClick={() => { updateExportableList(node); }}>
-      {node.name}
+    <div className={`selection-node selection-node--${node.status ? 'active' : ''}`}>
+      <input
+        className="selection-node-checkbox"
+        onClick={(): void => { updateExportableList(node); }}
+        type="checkbox"
+        aria-label={`Export ${node.name}`}
+        aria-checked={node.status}
+      />
+      <span
+        className="selecton-node-text"
+        onClick={(): void => { viewNode(node.id); }}
+        role="button"
+        tabIndex={0}
+        aria-label={`View ${node.name}`}
+      >
+        {node.name}
+      </span>
     </div>
   );
 };
